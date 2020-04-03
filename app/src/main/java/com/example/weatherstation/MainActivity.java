89835642519852844d811faff2509f62e16e5bc8
kirkadev <kirkadev@gmail.com>
2020-04-03 21:24:22 +0500

    1. Add request last known location of the device. 2. Layout edited. Unnecessary UI elements was removed.

--- app/src/main/java/com/example/weatherstation/MainActivity.java
+++ app/src/main/java/com/example/weatherstation/MainActivity.java
@@ -7,24 +7,16 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button pullMsgToServerBtn;
-    Button btnGetLocation;
-
-    EditText editTextLatitude;
-    EditText editTextLongitude;
-
-    TextView txtView_cityName;
-    TextView recievedFromServerTextView;
+    TextView textView_cityName;
+    TextView textView_degreesAtNow;
 
     InfoFromServer infoServer;
 
@@ -39,59 +31,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        infoServer = new InfoFromServer();
+        textView_cityName = findViewById(R.id.textView_cityName);
+        textView_degreesAtNow = findViewById(R.id.textView_degreesAtNow);
 
         locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         location = new CoarseLocation();
+        getLastKnownLocation();
 
-        editTextLatitude = findViewById(R.id.editTextLatitude);
-        editTextLongitude = findViewById(R.id.editTextLongitude);
+        infoServer = new InfoFromServer(location);
 
-        pullMsgToServerBtn = findViewById(R.id.button_get_weather);
-        btnGetLocation = findViewById(R.id.btnGetLocation);
-        recievedFromServerTextView = findViewById(R.id.recievedText);
-        txtView_cityName = findViewById(R.id.txtView_cityName);
-
-        // "Show the weather"
-        pullMsgToServerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void  onClick(View v) {
-
-                pojo = null;
-                getLocation();
-                infoServer = new InfoFromServer(location);
-
-                try {
-                    pojo = infoServer.WeatherAtNow();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                txtView_cityName.setText(pojo.getCityName());
-                recievedFromServerTextView.setText(getString(R.string.msgTempOverboard) + pojo.getTemp()+ "\n");
-            }
-        });
-
-        // Получение координат при нажатии на кнопку "GPS"
-        btnGetLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getLocation();
-                showLocation();
-            }
-        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.i("MainActivity", "onStart() called");
+        showWeatherInfo();
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
+
+        getLastKnownLocation();
         getLocation();
+        //showWeatherInfo();
     }
 
         LocationListener locationListener = new LocationListener() {
@@ -99,12 +64,17 @@ public class MainActivity extends AppCompatActivity {
         public void onLocationChanged(Location loc) {
             Log.i("Заходы в методы", "Зашел в onLocationChanged");
             Log.i("Latitude: ", String.valueOf(loc.getLatitude()));
-            Log.i("Longitude: ", String.valueOf(loc.getAltitude()));
+            Log.i("Longitude: ", String.valueOf(loc.getLongitude()));
 
             double latitude = loc.getLatitude();
             double longitude = loc.getLongitude();
 
             location.SetLocation(latitude, longitude);
+
+            Toast.makeText(getApplicationContext(), "lat-long: " + String.valueOf(latitude) + ", " + String.valueOf(longitude), Toast.LENGTH_LONG ).show();
+
+
+            showWeatherInfo();
         }
 
         @Override
@@ -133,14 +103,39 @@ public class MainActivity extends AppCompatActivity {
 
             return;
         }
-
-        locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 1, locationListener);
+            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1800000, 0, locationListener);
     }
 
-    private void showLocation()
+    private void getLastKnownLocation()
     {
-        editTextLatitude.setText(String.valueOf(location.getLatitude()));
-        editTextLongitude.setText(String.valueOf(location.getLongitude()));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            {
+                String[] arrStringPermissions =  {Manifest.permission.ACCESS_FINE_LOCATION};
+                ActivityCompat.requestPermissions(this, arrStringPermissions, 0);
+            }
+
+            return;
+        }
+        double latitude;
+        double longitude;
+
+        Location lastKnownLocation = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        latitude = lastKnownLocation.getLatitude();
+        longitude = lastKnownLocation.getLongitude();
+        location.SetLocation(latitude,longitude);
     }
 
+    private void showWeatherInfo()
+    {
+        infoServer = new InfoFromServer(location);
+
+        try {
+            pojo = infoServer.WeatherAtNow();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        textView_cityName.setText(pojo.getCityName());
+        textView_degreesAtNow.setText(pojo.getTemp()+ "°");
+        pojo = null;
+    }
 }
